753a8cfbfceb8a147e50970ff5b0ebf6f222487b
吕海涛 <git@lvht.net>
2015-11-25 11:32:12 +0800

    添加Laravel支持

---
+++ src/ServiceProvider.php
@@ -0,0 +1,14 @@
+<?php namespace Lvht\Pea;
+
+class ServiceProvider extends \Illuminate\Support\ServiceProvider
+{
+    public function register()
+    {
+        $this->app->singleton(Meta::class, function () {
+            return new RedisMeta($this->app->make('redis'));
+        });
+        $this->app->singleton(Cache::class, function () {
+            return new RedisCache($this->app->make('redis'));
+        });
+    }
+}
